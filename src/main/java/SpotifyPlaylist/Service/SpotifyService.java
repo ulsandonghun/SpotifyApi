b1887c2b72aa41ff91c7ca0657f1e186a09f90ab
ulsandonghun <donghoon141@naver.com>
2023-05-11 23:21:11 +0900

    [feat] : 가수이름과 앨범 동시에 검색 기능 추가

--- src/main/java/SpotifyPlaylist/Service/SpotifyService.java
+++ src/main/java/SpotifyPlaylist/Service/SpotifyService.java
@@ -23,11 +23,11 @@ public class SpotifyService {
             //accessToken()은 SpotifyConfig 에서 발급함
             .build();
 
-    public List<SearchResponseDto> search(String keyword) {
+    public List<SearchResponseDto> SearchByTrackname(String trackname) {
         List <SearchResponseDto> searchResponseDtoList = new ArrayList<>();
 
         try {
-            SearchTracksRequest searchTrackRequest = spotifyApi.searchTracks(keyword)
+            SearchTracksRequest searchTrackRequest = spotifyApi.searchTracks(trackname)
                     .limit(10)
                     .build();
 
@@ -55,5 +55,41 @@ public class SpotifyService {
         }
         return searchResponseDtoList;
     }
+
+
+    public List<SearchResponseDto> SearchByTracknameAndArtist(String trackname,String Artist) {
+        List <SearchResponseDto> searchResponseDtoList = new ArrayList<>();
+
+        try {
+            SearchTracksRequest searchTrackRequest = spotifyApi.searchTracks("track:" + trackname + " artist:" + Artist)
+                    .limit(10)
+                    .build();
+
+            Paging<Track> searchResult = searchTrackRequest.execute();
+            Track[] tracks = searchResult.getItems();
+
+            for (Track track : tracks) {
+                String title = track.getName();
+
+                AlbumSimplified album = track.getAlbum();
+                ArtistSimplified[] artists = album.getArtists();
+                String artistName = artists[0].getName();
+
+
+                Image[] images = album.getImages();
+                String imageUrl = (images.length > 0) ? images[0].getUrl() : "NO_IMAGE";
+
+                String albumName = album.getName();
+
+                searchResponseDtoList.add(DtoMapper.toSearchDto(artistName, title, albumName, imageUrl));
+            }
+
+        } catch (IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return searchResponseDtoList;
+    }
+
+
 
 }
